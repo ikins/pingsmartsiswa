9f0c9e1954bb9a02df23f7721f56892b14c033e2
Ikin <ikin.musyafa@gmail.com>
2019-02-07 03:49:09 +0700

    update function

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -7,10 +7,12 @@ var appsiswa =  angular.module('app', ['onsen','ipCookie','highcharts-ng','ngRou
 
 
 //server
-var _URL        = "http://pingsmart.gallerysneakers27.com/api/";
+//var _URL        = "http://pingsmart.gallerysneakers27.com/api/";
+//var BASE_URL        = "http://pingsmart.gallerysneakers27.com/";
 
 //local
-//var _URL        = "http://localhost:7777/apismart/api/";
+var _URL        = "http://localhost:7777/apismart/api/";
+var BASE_URL        = "http://localhost:7777/apismart/";
 
 
 var app = {
@@ -58,6 +60,18 @@ appsiswa.controller('getCurrentInfoWeek', ['$scope', '$http','ipCookie', functio
     $scope.logout = function(){
         window.localStorage.removeItem("member_id_siswa");
         window.localStorage.removeItem("token_siswa");
+        window.localStorage.removeItem("nis_siswa");
+        window.localStorage.removeItem("nisn_siswa");
+        //------------------------
+        window.localStorage.removeItem("Nama");
+        window.localStorage.removeItem("Kelas");
+        window.localStorage.removeItem("JumlahSiswa");
+        window.localStorage.removeItem("Rangking");
+        window.localStorage.removeItem("Point");
+        window.localStorage.removeItem("Lahir");
+        window.localStorage.removeItem("TglLahir");
+        window.localStorage.removeItem("Agama");
+        window.localStorage.removeItem("Avatar");
 
         fn.load('landing-page.html');
     };
@@ -90,6 +104,38 @@ appsiswa.controller('PageController', ['$scope', '$http','ipCookie', 'md5', func
                     window.localStorage.setItem("member_id_siswa", response.data[0].MemberId);
                     window.localStorage.setItem("token_siswa", response.data[0].Token);
                     window.localStorage.setItem("nis_siswa", response.data[0].NIS);
+                    window.localStorage.setItem("nisn_siswa", response.data[0].NISN);
+
+                    token_siswa = response.data[0].token_siswa;
+
+                    if (token_siswa != '') {
+
+                          token_siswa  = window.localStorage.getItem("token_siswa");
+                          nis_siswa    = window.localStorage.getItem("nis_siswa");
+
+                          $http.get( _URL+"siswa-profile?nis=" + nis_siswa + "&token=" + token_siswa)
+                              .success(function (response) {
+
+                              //---------------------SAVE LOCAL-----------------------
+                              window.localStorage.setItem("Nama", response.data[0].Nama);
+                              //--
+                              window.localStorage.setItem("Kelas", response.data[0].Kelas);
+                              window.localStorage.setItem("JumlahSiswa", response.data[0].JumlahSiswa);
+                              window.localStorage.setItem("Rangking", response.data[0].Rangking);
+                              //--
+                              window.localStorage.setItem("Point", response.data[0].Point);
+                              window.localStorage.setItem("Lahir", response.data[0].Lahir);
+                              window.localStorage.setItem("TglLahir", response.data[0].TglLahir);
+                              //--
+                              window.localStorage.setItem("Agama", response.data[0].Agama);
+                              window.localStorage.setItem("Avatar", response.data[0].Avatar);
+                              //--
+
+                              fn.load('dashboard.html');
+
+                          });
+
+                        }
 
                     fn.load('dashboard.html');
 
@@ -151,6 +197,25 @@ appsiswa.controller('PageController', ['$scope', '$http','ipCookie', 'md5', func
 
 appsiswa.controller('Pagedashboard', ['$scope', '$http', function($scope, $http) {
 
+    token_siswa  = window.localStorage.getItem("token_siswa");
+    nis_siswa    = window.localStorage.getItem("nis_siswa");
+
+    $scope.NIS = window.localStorage.getItem("nis_siswa");
+    $scope.NISN = window.localStorage.getItem("nisn_siswa");
+    //--------------------------------------------
+    $scope.Nama = window.localStorage.getItem("Nama");
+    $scope.Kelas = window.localStorage.getItem("Kelas");
+    $scope.JumlahSiswa = window.localStorage.getItem("JumlahSiswa");
+    //--------------------------------------------
+    $scope.Rangking = window.localStorage.getItem("Rangking");
+    $scope.Point = window.localStorage.getItem("Point");
+    $scope.Lahir = window.localStorage.getItem("Lahir");
+    //--------------------------------------------
+    $scope.TglLahir = window.localStorage.getItem("TglLahir");
+    $scope.Agama = window.localStorage.getItem("Agama");
+    $scope.Avatar = window.localStorage.getItem("Avatar");
+
+    $scope.URL_Avatar = BASE_URL + "/" + $scope.Avatar;
 
 }]);
 
@@ -168,23 +233,46 @@ appsiswa.controller('PageJadwal', ['$scope', '$http', function($scope, $http) {
 
 }]);
 
-appsiswa.controller('PageAgenda', ['$scope', '$http', function($scope, $http) {
+appsiswa.controller('PageNilaiUlangan', ['$scope', '$http', function($scope, $http) {
 
+    token_siswa  = window.localStorage.getItem("token_siswa");
+    nis_siswa    = window.localStorage.getItem("nis_siswa");
+
+    $http.get( _URL+"siswa-nilai-ulangan?nis=" + nis_siswa + "&token=" + token_siswa)
+        .success(function (response) {
+
+        $scope.list_nilai_ulangan = response.data;
+
+    });
 
 }]);
 
-appsiswa.controller('PageAkademik', ['$scope', '$http', function($scope, $http) {
-
-
-}]);
 
 appsiswa.controller('PagePengumuman', ['$scope', '$http', function($scope, $http) {
 
+    token_siswa  = window.localStorage.getItem("token_siswa");
+    nis_siswa    = window.localStorage.getItem("nis_siswa");
+
+    $http.get( _URL+"pengumuman?nis=" + nis_siswa + "&token=" + token_siswa)
+        .success(function (response) {
+
+        $scope.list_pengumuman = response.data;
+
+    });
 
 }]);
 
-appsiswa.controller('PageGaleri', ['$scope', '$http', function($scope, $http) {
+appsiswa.controller('PageAlbum', ['$scope', '$http', function($scope, $http) {
 
+    token_siswa  = window.localStorage.getItem("token_siswa");
+    nis_siswa    = window.localStorage.getItem("nis_siswa");
+
+    $http.get( _URL+"siswa-album?nis=" + nis_siswa + "&token=" + token_siswa)
+        .success(function (response) {
+
+        $scope.list_album = response.data;
+
+    });
 
 }]);
 
